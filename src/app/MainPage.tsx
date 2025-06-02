dc0bfdc4f0153419e0d7d2c2c3b461e79ac12f85
Bipin Thapa <yukichin638@gmail.com>
2025-06-02 22:09:47 +0545

    About Page Halfway made

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -3,12 +3,37 @@ import './MainPage.css';
 export default function MainPage() {
 
   return (
-    <div id='homepage-container'>
-      <img id="homepage-image" src="/homepage.png" alt="abcd" />
-      <div id='container'>
-        <div id='name-container'>Bipin Thapa</div>
-        <div id="typing">I am a developer</div>
+    <>
+      <div id='homepage-container'>
+        <img id="homepage-image" src="/homepage.png" alt="abcd" />
+        <div id='container'>
+          <div id='name-container'>Bipin Thapa</div>
+          <div id="typing">I am a developer</div>
+        </div>
       </div>
-    </div>
+      <div id='about-page'>
+        <div id='about-page-intro'>
+          <h2 id='about-page-intro-head'>
+            About
+          </h2>
+          <div>
+            My name is Bipin Thapa.
+          </div>
+        </div>
+        <div id='about-page-body'>
+          <div id='about-page-body-image-container'>
+            <img id='about-page-body-image' src="profile-cropped.jpg" alt="" />
+          </div>
+          <div id='about-page-body-content-container'>
+            <div id='about-page-body-content-header'>
+              Full Stack Developer & Data Scientist(Learning).
+            </div>
+            <div id='about-page-body-content-1'>
+              Test Text
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
